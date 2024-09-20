206a7835e83d386246680840d5ff33450ffb785c
Virginia Del Val <vdelval@salta02.local>
2024-09-20 13:52:13 -0300

    32 categorias aprob con Levin el 20/09/24

---
+++ data/style31cat.js
@@ -0,0 +1,346 @@
+// categorias: 145 Categotiras
+function getColorCategoria31(COD_LINK) {
+    switch (COD_LINK) {
+        case '102030415060': return { color: '#E4DFEC', category: '1' };
+        case '102030405060': return { color: '#CCC0DA', category: '2' };
+        case '102030415160': return { color: '#B1A0C7', category: '3' };
+        case '102030415061': return { color: '#B1A0C7', category: '3' };
+        case '102030425060': return { color: '#B1A0C7', category: '3' };
+        case '102030405160': return { color: '#60497A', category: '4' };
+        case '102030405061': return { color: '#60497A', category: '4' };
+        case '102030415161': return { color: '#403151', category: '5' };
+        case '102030405161': return { color: '#8064A2', category: '6' };
+        case '102131415060': return { color: '#EBF1DE', category: '7' };
+        case '102131405060': return { color: '#D8E4BC', category: '8' };
+        case '102131415061': return { color: '#C4D79B', category: '9' };
+        case '102131415160': return { color: '#C4D79B', category: '9' };
+        case '102131425060': return { color: '#C4D79B', category: '9' };
+        case '102131405160': return { color: '#C4D79B', category: '9' };
+        case '102131405061': return { color: '#C4D79B', category: '9' };
+        case '102131415161': return { color: '#76933C', category: '10' };
+        case '102131425160': return { color: '#76933C', category: '10' };
+        case '102131425061': return { color: '#76933C', category: '10' };
+        case '102131405161': return { color: '#4F6228', category: '11' };
+        case '102131425161': return { color: '#C4D79B', category: '12' };
+        case '102132415060': return { color: '#FCD5B4', category: '13' };
+        case '102132405060': return { color: '#FABF8F', category: '14' };
+        case '102132415061': return { color: '#E26B0A', category: '15' };
+        case '102132415160': return { color: '#E26B0A', category: '15' };
+        case '102032405061': return { color: '#E26B0A', category: '15' };
+        case '102132405061': return { color: '#974706', category: '16' };
+        case '102132405160': return { color: '#974706', category: '16' };
+        case '102132415161': return { color: '#FABF8F', category: '17' };
+        case '102132425061': return { color: '#FABF8F', category: '17' };
+        case '102132425160': return { color: '#FABF8F', category: '17' };
+        case '102332425160': return { color: '#FABF8F', category: '17' };
+        case '102132405161': return { color: '#FDE9D9', category: '18' };
+        case '102132425161': return { color: '#F79646', category: '19' };
+        case '102133415060': return { color: '#DAEEF3', category: '20' };
+        case '102233415060': return { color: '#DAEEF3', category: '20' };
+        case '102133405060': return { color: '#B7DEE8', category: '21' };
+        case '102133415061': return { color: '#92CDDC', category: '22' };
+        case '102133415160': return { color: '#92CDDC', category: '22' };
+        case '102133425060': return { color: '#92CDDC', category: '22' };
+        case '102233415061': return { color: '#92CDDC', category: '22' };
+        case '102133405061': return { color: '#92CDDC', category: '22' };
+        case '102133405160': return { color: '#92CDDC', category: '22' };
+        case '102133415161': return { color: '#31869B', category: '23' };
+        case '102133425061': return { color: '#31869B', category: '23' };
+        case '102133425160': return { color: '#31869B', category: '23' };
+        case '102233415161': return { color: '#31869B', category: '23' };
+        case '102233425061': return { color: '#31869B', category: '23' };
+        case '102333405061': return { color: '#31869B', category: '23' };
+        case '102333405160': return { color: '#31869B', category: '23' };
+        case '102133405161': return { color: '#31869B', category: '23' };
+        case '112133415061': return { color: '#31869B', category: '23' };
+        case '122133415061': return { color: '#31869B', category: '23' };
+        case '122133415160': return { color: '#31869B', category: '23' };
+        case '122133425060': return { color: '#31869B', category: '23' };
+        case '122233415061': return { color: '#31869B', category: '23' };
+        case '122333405060': return { color: '#31869B', category: '23' };
+        case '102333415060': return { color: '#215967', category: '24' };
+        case '102333415160': return { color: '#215967', category: '24' };
+        case '102333405060': return { color: '#215967', category: '24' };
+        case '102333415061': return { color: '#92CDDC', category: '25' };
+        case '102133425161': return { color: '#92CDDC', category: '25' };
+        case '102333405161': return { color: '#92CDDC', category: '25' };
+        case '102333415161': return { color: '#92CDDC', category: '25' };
+        case '102233425161': return { color: '#92CDDC', category: '25' };
+        case '102333425061': return { color: '#92CDDC', category: '25' };
+        case '102333425160': return { color: '#92CDDC', category: '25' };
+        case '112133425061': return { color: '#92CDDC', category: '25' };
+        case '112133405061': return { color: '#92CDDC', category: '25' };
+        case '112133415161': return { color: '#92CDDC', category: '25' };
+        case '112133405161': return { color: '#92CDDC', category: '25' };
+        case '122233415161': return { color: '#92CDDC', category: '25' };
+        case '122333415061': return { color: '#92CDDC', category: '25' };
+        case '122133425061': return { color: '#92CDDC', category: '25' };
+        case '122133405161': return { color: '#92CDDC', category: '25' };
+        case '122133415161': return { color: '#92CDDC', category: '25' };
+        case '102333425161': return { color: '#B7DEE8', category: '26' };
+        case '112133425161': return { color: '#B7DEE8', category: '26' };
+        case '122133425161': return { color: '#B7DEE8', category: '26' };
+        case '122333415161': return { color: '#B7DEE8', category: '26' };
+        case '102134415060': return { color: '#DDD9C4', category: '27' };
+        case '102134405060': return { color: '#DDD9C4', category: '27' };
+        case '102134415061': return { color: '#C4BD97', category: '28' };
+        case '102134425060': return { color: '#C4BD97', category: '28' };
+        case '102134405061': return { color: '#C4BD97', category: '28' };
+        case '102134405160': return { color: '#C4BD97', category: '28' };
+        case '102134415160': return { color: '#C4BD97', category: '28' };
+        case '102234415061': return { color: '#C4BD97', category: '28' };
+        case '102334405060': return { color: '#C4BD97', category: '28' };
+        case '102334415060': return { color: '#C4BD97', category: '28' };
+        case '112134415060': return { color: '#C4BD97', category: '28' };
+        case '112134405060': return { color: '#C4BD97', category: '28' };
+        case '122134415060': return { color: '#C4BD97', category: '28' };
+        case '102334415061': return { color: '#948A54', category: '29' };
+        case '102334415160': return { color: '#948A54', category: '29' };
+        case '102134415161': return { color: '#948A54', category: '29' };
+        case '102134425061': return { color: '#948A54', category: '29' };
+        case '102134425160': return { color: '#948A54', category: '29' };
+        case '102234415161': return { color: '#948A54', category: '29' };
+        case '102334405061': return { color: '#948A54', category: '29' };
+        case '102334405160': return { color: '#948A54', category: '29' };
+        case '112134415160': return { color: '#948A54', category: '29' };
+        case '112134415061': return { color: '#948A54', category: '29' };
+        case '112234415061': return { color: '#948A54', category: '29' };
+        case '112334405060': return { color: '#948A54', category: '29' };
+        case '122134415160': return { color: '#948A54', category: '29' };
+        case '122134415061': return { color: '#948A54', category: '29' };
+        case '122234415061': return { color: '#948A54', category: '29' };
+        case '102134405161': return { color: '#948A54', category: '29' };
+        case '122134405160': return { color: '#948A54', category: '29' };
+        case '112134405061': return { color: '#948A54', category: '29' };
+        case '122134405061': return { color: '#948A54', category: '29' };
+        case '102334415161': return { color: '#494529', category: '30' };
+        case '102334425061': return { color: '#494529', category: '30' };
+        case '102334425160': return { color: '#494529', category: '30' };
+        case '112034415161': return { color: '#494529', category: '30' };
+        case '122334415061': return { color: '#494529', category: '30' };
+        case '112334415061': return { color: '#494529', category: '30' };
+        case '102134425161': return { color: '#494529', category: '30' };
+        case '102234425161': return { color: '#494529', category: '30' };
+        case '112234415161': return { color: '#494529', category: '30' };
+        case '112134425061': return { color: '#494529', category: '30' };
+        case '122134425061': return { color: '#494529', category: '30' };
+        case '112134415161': return { color: '#494529', category: '30' };
+        case '102334405161': return { color: '#494529', category: '30' };
+        case '112334405061': return { color: '#494529', category: '30' };
+        case '112134425160': return { color: '#494529', category: '30' };
+        case '122134415161': return { color: '#494529', category: '30' };
+        case '122234415161': return { color: '#494529', category: '30' };
+        case '112134405161': return { color: '#494529', category: '30' };
+        case '122134405161': return { color: '#494529', category: '30' };
+        case '102334425161': return { color: '#C4BD97', category: '31' };
+        case '112334415161': return { color: '#C4BD97', category: '31' };
+        case '112334425061': return { color: '#C4BD97', category: '31' };
+        case '112334425160': return { color: '#C4BD97', category: '31' };
+        case '112134425161': return { color: '#C4BD97', category: '31' };
+        case '122134425161': return { color: '#C4BD97', category: '31' };
+        case '122234425161': return { color: '#C4BD97', category: '31' };
+        case '122334405161': return { color: '#C4BD97', category: '31' };
+        case '112234425161': return { color: '#C4BD97', category: '31' };
+        case '112334405161': return { color: '#C4BD97', category: '31' };
+        case '122334415161': return { color: '#C4BD97', category: '31' };
+        case '122334425061': return { color: '#C4BD97', category: '31' };
+        case '112334425161': return { color: '#31869B', category: '32' };
+        case '122334425161': return { color: '#31869B', category: '32' };
+        default: return { color: "black" }; // Default color if CATEGORIA is not found
+    }
+}
+
+
+function styleCategoria31(feature) {
+    const { color } = getColorCategoria31(feature.properties.COD_LINK);
+    return {
+        color: color,
+        weight: 4.5,
+        opacity: 0.7,
+        dashArray: "0",
+        fillOpacity: 0.7,
+    };
+}
+
+
+function createLegendCategoria31() {
+    var legend = L.control({ position: "bottomright" });
+
+    legend.onAdd = function () {
+        var div = L.DomUtil.create("div", "info legend");
+        var labels = [];
+
+        // Array con los estilos y sus etiquetas correspondientes
+        var styles = [
+            { label: "Categorias", color: "" },
+            { label: "1", color: "#E4DFEC" },
+            { label: "2", color: "#CCC0DA" },
+            { label: "3", color: "#B1A0C7" },
+            { label: "3", color: "#B1A0C7" },
+            { label: "3", color: "#B1A0C7" },
+            { label: "4", color: "#60497A" },
+            { label: "4", color: "#60497A" },
+            { label: "5", color: "#403151" },
+            { label: "6", color: "#8064A2" },
+            { label: "7", color: "#EBF1DE" },
+            { label: "8", color: "#D8E4BC" },
+            { label: "9", color: "#C4D79B" },
+            { label: "9", color: "#C4D79B" },
+            { label: "9", color: "#C4D79B" },
+            { label: "9", color: "#C4D79B" },
+            { label: "9", color: "#C4D79B" },
+            { label: "10", color: "#76933C" },
+            { label: "10", color: "#76933C" },
+            { label: "10", color: "#76933C" },
+            { label: "11", color: "#4F6228" },
+            { label: "12", color: "#C4D79B" },
+            { label: "13", color: "#FCD5B4" },
+            { label: "14", color: "#FABF8F" },
+            { label: "15", color: "#E26B0A" },
+            { label: "15", color: "#E26B0A" },
+            { label: "15", color: "#E26B0A" },
+            { label: "16", color: "#974706" },
+            { label: "16", color: "#974706" },
+            { label: "17", color: "#FABF8F" },
+            { label: "17", color: "#FABF8F" },
+            { label: "17", color: "#FABF8F" },
+            { label: "17", color: "#FABF8F" },
+            { label: "18", color: "#FDE9D9" },
+            { label: "19", color: "#F79646" },
+            { label: "20", color: "#DAEEF3" },
+            { label: "20", color: "#DAEEF3" },
+            { label: "21", color: "#B7DEE8" },
+            { label: "22", color: "#92CDDC" },
+            { label: "22", color: "#92CDDC" },
+            { label: "22", color: "#92CDDC" },
+            { label: "22", color: "#92CDDC" },
+            { label: "22", color: "#92CDDC" },
+            { label: "22", color: "#92CDDC" },
+            { label: "23", color: "#31869B" },
+            { label: "23", color: "#31869B" },
+            { label: "23", color: "#31869B" },
+            { label: "23", color: "#31869B" },
+            { label: "23", color: "#31869B" },
+            { label: "23", color: "#31869B" },
+            { label: "23", color: "#31869B" },
+            { label: "23", color: "#31869B" },
+            { label: "23", color: "#31869B" },
+            { label: "23", color: "#31869B" },
+            { label: "23", color: "#31869B" },
+            { label: "23", color: "#31869B" },
+            { label: "23", color: "#31869B" },
+            { label: "23", color: "#31869B" },
+            { label: "24", color: "#215967" },
+            { label: "24", color: "#215967" },
+            { label: "24", color: "#215967" },
+            { label: "25", color: "#92CDDC" },
+            { label: "25", color: "#92CDDC" },
+            { label: "25", color: "#92CDDC" },
+            { label: "25", color: "#92CDDC" },
+            { label: "25", color: "#92CDDC" },
+            { label: "25", color: "#92CDDC" },
+            { label: "25", color: "#92CDDC" },
+            { label: "25", color: "#92CDDC" },
+            { label: "25", color: "#92CDDC" },
+            { label: "25", color: "#92CDDC" },
+            { label: "25", color: "#92CDDC" },
+            { label: "25", color: "#92CDDC" },
+            { label: "25", color: "#92CDDC" },
+            { label: "25", color: "#92CDDC" },
+            { label: "25", color: "#92CDDC" },
+            { label: "25", color: "#92CDDC" },
+            { label: "26", color: "#B7DEE8" },
+            { label: "26", color: "#B7DEE8" },
+            { label: "26", color: "#B7DEE8" },
+            { label: "26", color: "#B7DEE8" },
+            { label: "27", color: "#DDD9C4" },
+            { label: "27", color: "#DDD9C4" },
+            { label: "28", color: "#C4BD97" },
+            { label: "28", color: "#C4BD97" },
+            { label: "28", color: "#C4BD97" },
+            { label: "28", color: "#C4BD97" },
+            { label: "28", color: "#C4BD97" },
+            { label: "28", color: "#C4BD97" },
+            { label: "28", color: "#C4BD97" },
+            { label: "28", color: "#C4BD97" },
+            { label: "28", color: "#C4BD97" },
+            { label: "28", color: "#C4BD97" },
+            { label: "28", color: "#C4BD97" },
+            { label: "29", color: "#948A54" },
+            { label: "29", color: "#948A54" },
+            { label: "29", color: "#948A54" },
+            { label: "29", color: "#948A54" },
+            { label: "29", color: "#948A54" },
+            { label: "29", color: "#948A54" },
+            { label: "29", color: "#948A54" },
+            { label: "29", color: "#948A54" },
+            { label: "29", color: "#948A54" },
+            { label: "29", color: "#948A54" },
+            { label: "29", color: "#948A54" },
+            { label: "29", color: "#948A54" },
+            { label: "29", color: "#948A54" },
+            { label: "29", color: "#948A54" },
+            { label: "29", color: "#948A54" },
+            { label: "29", color: "#948A54" },
+            { label: "29", color: "#948A54" },
+            { label: "29", color: "#948A54" },
+            { label: "29", color: "#948A54" },
+            { label: "30", color: "#494529" },
+            { label: "30", color: "#494529" },
+            { label: "30", color: "#494529" },
+            { label: "30", color: "#494529" },
+            { label: "30", color: "#494529" },
+            { label: "30", color: "#494529" },
+            { label: "30", color: "#494529" },
+            { label: "30", color: "#494529" },
+            { label: "30", color: "#494529" },
+            { label: "30", color: "#494529" },
+            { label: "30", color: "#494529" },
+            { label: "30", color: "#494529" },
+            { label: "30", color: "#494529" },
+            { label: "30", color: "#494529" },
+            { label: "30", color: "#494529" },
+            { label: "30", color: "#494529" },
+            { label: "30", color: "#494529" },
+            { label: "30", color: "#494529" },
+            { label: "30", color: "#494529" },
+            { label: "31", color: "#C4BD97" },
+            { label: "31", color: "#C4BD97" },
+            { label: "31", color: "#C4BD97" },
+            { label: "31", color: "#C4BD97" },
+            { label: "31", color: "#C4BD97" },
+            { label: "31", color: "#C4BD97" },
+            { label: "31", color: "#C4BD97" },
+            { label: "31", color: "#C4BD97" },
+            { label: "31", color: "#C4BD97" },
+            { label: "31", color: "#C4BD97" },
+            { label: "31", color: "#C4BD97" },
+            { label: "31", color: "#C4BD97" },
+            { label: "32", color: "#31869B" },
+            { label: "32", color: "#31869B" }
+        ];
+        // Genera el contenido HTML de la leyenda
+        styles.forEach(function (style) {
+            labels.push(
+                '<i style="background:' + style.color + '"></i> ' + style.label
+            );
+        });
+
+        div.innerHTML = labels.join("<br>");
+        return div;
+    };
+
+    // Función para mostrar/ocultar la leyenda
+    function toggleLegend() {
+        if (map.hasLayer(EjesCategoria31)) {
+            legend.addTo(map);
+        } else {
+            legend.remove();
+        }
+    }
+
+  // Eliminar la leyenda al inicio
+  legend.remove();
+  // Escucha el cambio de estado de la capa
+  map.on("overlayadd overlayremove", toggleLegend);
+}
